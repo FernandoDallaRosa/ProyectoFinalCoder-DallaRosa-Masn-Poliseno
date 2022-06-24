fd70b8b179cf92c5ea800f3f0c8d2cc40eefeb05
Fernando Dalla Rosa <dallarosafernando@gmail.com>
2022-06-24 16:16:38 -0300

    Templates

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -10,10 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+TEMPLATES_DIRS = os.path.join(BASE_DIR,'templates')
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -31,6 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "blog",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +59,7 @@ ROOT_URLCONF = 'proyecto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIRS],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- proyecto/urls.py
+++ proyecto/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from xml.etree.ElementInclude import include
+from django import views
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from blog import views
 
 urlpatterns = [
+    path(" ", include("blog.urls")),
     path('admin/', admin.site.urls),
+    path('', views.index, name="homepage"),
 ]
+
+
+
